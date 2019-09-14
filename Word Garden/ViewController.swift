792d2d96c4146732e451ee1e96862f645b1305d7
Daniel Yang <yangafc@bc.edu>
2019-09-13 21:48:48 -0400

    3.2 complete (with print statements)

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userGuessLabel: UILabel!
+    @IBOutlet weak var guessedLetterField: UITextField!
+    @IBOutlet weak var guessLetterButton: UIButton!
+    @IBOutlet weak var guessCountLabel: UILabel!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var flowerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("in viewDidLoad, is the guessedLetterField the first responder?", guessedLetterField.isFirstResponder)
     }
-
-
+    
+    func updateUIAfterGuess() {
+        guessedLetterField.resignFirstResponder()
+        guessedLetterField.text = ""
+    }
+    @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
+        print("Hey the guessed letter field changed")
+    }
+    
+    @IBAction func doneKeyPressed(_ sender: UITextField) {
+        print("in doneKeyPressed, is the guessedLetterField the first responder before updateUIAfterGuess?", guessedLetterField.isFirstResponder)
+        updateUIAfterGuess()
+        print("in doneKeyPressed, is the guessedLetterField the first responder after updateUIAfterGuess?", guessedLetterField.isFirstResponder)
+    }
+    
+    @IBAction func guessLetterButtonPressed(_ sender: UIButton) {
+        print("in guessLetterButtonPressed, is the guessedLetterField the first responder before updateUIAfterGuess?", guessedLetterField.isFirstResponder)
+        updateUIAfterGuess()
+        print("in guessLetterButtonPressed, is the guessedLetterField the first responder after updateUIAfterGuess?", guessedLetterField.isFirstResponder)
+    }
+    
+    @IBAction func playAgainButtonPressed(_ sender: UIButton) {
+    }
+    
 }
 
